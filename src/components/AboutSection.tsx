c7049235261128ffde8c5baa5a76738b61ba6f24
Krushna Padole <142484683+padolekrushna@users.noreply.github.com>
2026-02-21 20:16:57 +0530

    Updated B.Sc. Data Science education entry and removed dollar amount from projects.

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,11 +1,11 @@
-// AboutSection.tsx
-// Other code here...
+{/* Education Section */}
 
-// Education Section
-// Line 156: Update B.Sc. Data Science dates
-const education = [
-  { degree: 'B.Sc. Data Science', dates: '2022 - 2025' },
-  // Other education entries...
-];
+// B.Sc. Data Science
+Entry: 2022 - 2025 
 
-// Other code continues...
+{/* Other content */}
+
+// Projects
+High-impact enterprise projects valued at 
+
+{/* Further content */}
